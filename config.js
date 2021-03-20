4ff06c76162fe62ba0758fcd07634fbbd1cc0c42
daniel <luis.daniel132@outlook.es>
2021-03-20 12:21:44 -0600

    CONFIG CHANGES

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ const dotenv = require('dotenv').config();
 module.exports = {
     PORT: process.env.PORT || 3001,
     DB_URL: process.env.DB_URL || 'mongodb://localhost:27017/food',
-    NODE_ENV: process.env.NODE_ENV || 'development',
+    NODE_ENV: process.env.NODE_ENV || 'production',
   }
