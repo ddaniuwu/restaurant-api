1ce2d64b2aa422b98e27429762790effc69004ac
daniel <luis.daniel132@outlook.es>
2021-03-19 23:15:51 -0600

    My config file change

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
 var mongoose = require('mongoose')
 var app = require('./app')
 var config  = require('./config')
-//var express = require('express') //llamamos a Express
 
-//archivos estaticos y estilos
-//app.set('view engine', 'pug'); //renderiza al archivo index.pug en views
-//app.use('/static', express.static('public')); //agrega archivos estaticos
-
-//app.get('/', function (req, res) { //meotodo para renderizar a index.pug 
-  //res.render('index', { Platillo: 'Carne asada', Bebida: 'Refresco!' , Postre: 'Helado'});
-  //});
-
-
-mongoose.connect('mongodb://localhost:27017/food', function(err,res){
+mongoose.connect(config.db, function(err,res){
     if(err) {
         return console.log('Error')
     }
